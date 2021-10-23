bfa574159a62fbe7d3d3b2656de47e4ed9837c55
danber2502 <dantezzxddd@gmail.com>
2021-10-23 10:25:35 -0500

    camara2

--- Assets/Scripts/MovimientoCamara.cs
+++ Assets/Scripts/MovimientoCamara.cs
@@ -5,46 +5,46 @@ using UnityEngine;
 public class MovimientoCamara : MonoBehaviour
 {
 
-    [SerializeField]
-    float speed;
+    [SerializeField] private Camera _minimapCamera;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _borderSize;
+    [SerializeField] private Vector2 _borderLimit;
+    [SerializeField] private float _scrollSpeed;
+    [SerializeField] private float _minHeight;
+    [SerializeField] private float _maxHeight;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A)) {
+        Vector3 position = transform.position;
 
-            transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, 
-                transform.position.y, transform.position.z);
-        
-        }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - _borderSize)
         {
-
-            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime,
-                transform.position.y, transform.position.z);
-
+            position.z += _speed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <= _borderSize)
         {
-
-            transform.position = new Vector3(transform.position.x,
-                transform.position.y, transform.position.z + speed * Time.deltaTime);
-
+            position.z -= _speed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - _borderSize)
         {
-
-            transform.position = new Vector3(transform.position.x,
-                transform.position.y, transform.position.z - speed * Time.deltaTime);
-
+            position.x += _speed * Time.deltaTime;
         }
+
+        if (Input.GetKey(KeyCode.A) || Input.mousePosition.x <= _borderSize)
+        {
+            position.x -= _speed * Time.deltaTime;
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        position.y += -scroll * _scrollSpeed * 100f * Time.deltaTime;
+        position.y = Mathf.Clamp(position.y, _minHeight, _maxHeight);
+        // _minimapCamera.orthographicSize = position.y; // For now I comment it out. It might look better without zooming it.
+
+        position.x = Mathf.Clamp(position.x, -_borderLimit.x, _borderLimit.x);
+        position.z = Mathf.Clamp(position.z, -_borderLimit.y, _borderLimit.y);
+
+        transform.position = position;
     }
 }
