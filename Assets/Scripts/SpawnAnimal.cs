19cdae32bfa950c5399d6165d69b8acba6175d8b
danber2502 <dantezzxddd@gmail.com>
2021-11-30 20:26:51 -0500

    .

--- Assets/Scripts/SpawnAnimal.cs
+++ Assets/Scripts/SpawnAnimal.cs
@@ -4,53 +4,52 @@ using UnityEngine;
 
 public class SpawnAnimal : MonoBehaviour
 {
+    //lista de animales que reapareceran
     [SerializeField]
-    GameObject prefab;
+    GameObject[] animales;
 
+    //tiempo en el que reaparecera el animal
     [SerializeField]
-    int tiempoSpawn;
+    float tiempoRespawn;
 
-    [SerializeField]
-    int cantidadMaxima;
+    float tiempo=0;
 
-    List<GameObject> animales = new List<GameObject>();
-    List<Transform> pos = new List<Transform>();
     // Start is called before the first frame update
     void Start()
     {
-        //se crea los animales del inicio
-        for (int i = 0; i < cantidadMaxima;i++) {
 
-            animales.Add(Instantiate(prefab, transform.position, transform.rotation));
-            animales[i].transform.rotation = Quaternion.Euler(transform.rotation.x, 180, transform.rotation.z);
-            
-
-        }
-
-        //se cambian las posiciones de inicio
-        animales[0].transform.position = new Vector3(transform.position.x + 1.5f, transform.position.y, transform.position.z);
-        animales[1].transform.position = new Vector3(transform.position.x - 1.5f, transform.position.y, transform.position.z);
-
-        //se guarda las 3 posiciones en la lista para los siguientes spawns
-        for (int i = 0; i < cantidadMaxima; i++) {
-
-            pos.Add(animales[i].transform);
-
-        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q)) {
 
-            Debug.Log(animales.Count);
-
-        }
-        if (Input.GetKeyDown(KeyCode.E))
+        for (int i = 0; i < animales.Length; i++)
         {
-            Debug.Log(animales.Count);
+            //verifica si algun animal esta muerto(desactivado)
+            if (animales[i].activeInHierarchy == false)
+            {
+                //si el animal esta desactivado entonces el tiempo empieza a correr
+                tiempo += Time.deltaTime;
+                //si el tiempo es mayor al establecido entonces el animal reaparece
+                if (tiempo >= tiempoRespawn)
+                {
+                    animales[i].transform.GetChild(0).gameObject.GetComponent<Animal>().comidaVariante =
+                        animales[i].transform.GetChild(0).gameObject.GetComponent<Animal>().comidaObtenida;
+
+                    animales[i].transform.GetChild(0).gameObject.GetComponent<Animal>().vidaVariante =
+                        animales[i].transform.GetChild(0).gameObject.GetComponent<Animal>().vida;
+
+                    animales[i].GetComponent<ComportamientoAnimal>().speedVariable =
+                        animales[i].GetComponent<ComportamientoAnimal>().speedFijo;
+
+                    animales[i].SetActive(true);
+
+                    tiempo = 0;
+                }
+
+            }
 
         }
     }
